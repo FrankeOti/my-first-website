515f7e5892ce7bb8fd257787ebc5376c23383177
W Frankie <wfrank2725@outlook.com>
2026-01-29 10:42:05 +0300

    First website upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Wait until the page loads
+document.addEventListener("DOMContentLoaded", function() {
+    // Create a button
+    const btn = document.createElement("button");
+    btn.textContent = "Do Not Click Me!";
+    btn.style.padding = "10px 20px";
+    btn.style.fontSize = "16px";
+    btn.style.cursor = "pointer";
+    
+    // Add a click event
+    btn.addEventListener("click", function() {
+        alert("You clicked the button, you Buffoon!");
+    });
+    
+    // Add button to the top of main
+    document.querySelector("main").prepend(btn);
+});
